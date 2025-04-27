2534f032493ef88e58cff85179955e249d5bf5db
PRAVEEN BOLLA <140931654+Praveen220622@users.noreply.github.com>
2025-04-27 18:50:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,3 +28,6 @@ app.use("/api/transactions", transactionRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.use('/',(req,res)=>{
+    res.send("Welcome")
+})
